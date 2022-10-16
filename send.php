fd19b2d458e19e4f793708431e0817345c4f5714
pramudithaSJ <IT20620202@my.sliit.lk>
2022-10-16 22:53:50 +0530

    all

---
+++ send.php
@@ -0,0 +1,11 @@
+<?php
+use PHPMailer\PHPmaile\PHPMailer;
+use PHPMailer\PHPmaile\Exeception;
+
+require 'PhpMailer/src/Execption.php';
+require 'PhpMailer/src/PHPMailer.php';
+require 'PhpMailer/src/SMTP.php';
+
+if(isset($_POST["send"]))
+
+?>
